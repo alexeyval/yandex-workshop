ce759dfae2bef6158a3fd347b3592754f276b003
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-01-18 01:39:49 +0500

    add task-02-list-node

---
+++ sprint-02/v-task-02-list-node.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	Solution(nil)
+
+	nodes := []ListNode{
+		{
+			data: "1",
+		},
+		{
+			data: "2",
+		},
+		{
+			data: "3",
+		},
+		{
+			data: "4",
+		},
+	}
+	for i := 0; i < len(nodes)-1; i++ {
+		nodes[i].next = &nodes[i+1]
+	}
+
+	Solution(nil)
+	Solution(&nodes[0])
+}
+
+type ListNode struct {
+	data string
+	next *ListNode
+}
+
+func Solution(head *ListNode) {
+	node := head
+	for node != nil {
+		fmt.Print(node.data + " -> ")
+		node = node.next
+	}
+	fmt.Print("None")
+	fmt.Println()
+}
