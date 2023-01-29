6aaf95986073ab1623400c18828255aef64c5350
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-01-29 23:06:59 +0500

    task 12: module fibonacci

--- sprint-02/v-task-12-module-fibonacci.go
+++ sprint-02/v-task-12-module-fibonacci.go
@@ -10,10 +10,12 @@ func main() {
 	fmt.Println(moduleFibonacci(11, 1))
 }
 
-func moduleFibonacci(n, k int) int {
-	if n <= 1 {
-		return n
+func moduleFibonacci(n int, k float64) int {
+	cache := [2]int{0, 1}
+
+	for i := 2; i <= n; i++ {
+		cache[i%2] = (cache[0] + cache[1]) % int(math.Pow(10, k))
 	}
-	n = (moduleFibonacci(n-1, k) + moduleFibonacci(n-2, k)) % int(math.Pow(10, float64(k)))
-	return n
+
+	return cache[n%2]
 }
