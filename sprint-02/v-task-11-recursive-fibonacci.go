aa49616cec0c3a7243e96b4a81b29fd217ffac03
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-01-29 23:28:37 +0500

    task 11: recursive fibonacci

--- sprint-02/v-task-11-recursive-fibonacci.go
+++ sprint-02/v-task-11-recursive-fibonacci.go
@@ -1,15 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println(recursiveFibonacci(4))
-	fmt.Println(recursiveFibonacci(1))
+	fmt.Println(recursiveFibonacci(4, nil))
+	fmt.Println(recursiveFibonacci(1, nil))
+
+	fmt.Println("тест производительности")
+	start := time.Now()
+	fmt.Println(recursiveFibonacci(100, nil))
+	d := time.Since(start)
+	fmt.Printf("\nВремя выполнения = %v\n\n", d)
 }
 
-func recursiveFibonacci(n int) int {
-	if n <= 1 {
-		return n
+// https://tproger.ru/problems/finding-fibonacci/
+func recursiveFibonacci(n int, cache []int) int {
+	if cache == nil {
+		cache = make([]int, n+1)
 	}
-	return recursiveFibonacci(n-1) + recursiveFibonacci(n-2)
+	if cache[n] == 0 {
+		if n <= 1 {
+			cache[n] = 1
+		} else {
+			cache[n] = recursiveFibonacci(n-1, cache) + recursiveFibonacci(n-2, cache)
+		}
+	}
+
+	return cache[n]
 }
