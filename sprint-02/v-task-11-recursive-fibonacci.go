fe21325550eade00b70dddc420c679e468d65e65
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-01-30 14:13:40 +0500

    task 11: delete nil

--- sprint-02/v-task-11-recursive-fibonacci.go
+++ sprint-02/v-task-11-recursive-fibonacci.go
@@ -6,18 +6,22 @@ import (
 )
 
 func main() {
-	fmt.Println(recursiveFibonacci(4, nil))
-	fmt.Println(recursiveFibonacci(1, nil))
+	fmt.Println(recursiveFibonacci(4))
+	fmt.Println(recursiveFibonacci(1))
 
 	fmt.Println("тест производительности")
 	start := time.Now()
-	fmt.Println(recursiveFibonacci(100, nil))
+	fmt.Println(recursiveFibonacci(100))
 	d := time.Since(start)
 	fmt.Printf("\nВремя выполнения = %v\n\n", d)
 }
 
 // https://tproger.ru/problems/finding-fibonacci/
-func recursiveFibonacci(n int, cache []int) int {
+func recursiveFibonacci(n int) int {
+	return recursiveFibonacciMain(n, nil)
+}
+
+func recursiveFibonacciMain(n int, cache []int) int {
 	if cache == nil {
 		cache = make([]int, n+1)
 	}
@@ -25,7 +29,7 @@ func recursiveFibonacci(n int, cache []int) int {
 		if n <= 1 {
 			cache[n] = n
 		} else {
-			cache[n] = recursiveFibonacci(n-1, cache) + recursiveFibonacci(n-2, cache)
+			cache[n] = recursiveFibonacciMain(n-1, cache) + recursiveFibonacciMain(n-2, cache)
 		}
 	}
 
