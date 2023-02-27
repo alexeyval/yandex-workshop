6a56198dbfbf6c6201ce488a079b38ccb93c7d7b
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-02-28 04:57:07 +0500

    OK

---
+++ sprint-03/v-task-01-A.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	for _, test := range []struct {
+		in   int
+		want string
+	}{
+		{3, "((()))\n(()())\n(())()\n()(())\n()()()\n"},
+		{2, "(())\n()()\n"},
+	} {
+		got := A(test.in*2, "", 0, new(string))
+		if got != test.want {
+			fmt.Printf("%v: got %q; want %q\n", test.in, got, test.want)
+		}
+	}
+}
+
+func A(n int, prefix string, count int, res *string) string {
+	if n == 0 {
+		*res += prefix + "\n"
+		return *res
+	}
+
+	if count <= n/2 {
+		A(n-1, prefix+"(", count+1, res)
+	}
+	if count > 0 {
+		A(n-1, prefix+")", count-1, res)
+	}
+	return *res
+}
